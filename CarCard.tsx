32ca81f43afe7d459274129306a991870e5037ec
Savil09 <savilseldonpinto@gmail.com>
2026-04-15 13:09:46 -0400

    Update import path for useDreamGarage

--- CarCard.tsx
+++ CarCard.tsx
@@ -3,7 +3,7 @@ import { motion } from 'motion/react';
 import { Heart, Zap, Gauge, ArrowRight } from 'lucide-react';
 import { Car } from '../types';
 import { cn } from '../utils';
-import { useDreamGarage } from '../useDreamGarage';
+import { useDreamGarage } from './useDreamGarage';
 
 interface CarCardProps {
   car: Car;
